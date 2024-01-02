0ee5633bfe7dedece9ca1f1cceb6ba8fd53952da
nschainblatt <nschainblatt@gmail.com>
2024-01-01 22:33:00 -0500

    Code along for chapter 7

---
+++ code-along/library/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+use crate::front_of_house::hosting;
+
+mod customer {
+    pub fn eat_at_restaurant() {
+        super::hosting::add_to_waitlist();
+    }
+}
--- code-along/resturant/src/lib.rs
+++ code-along/resturant/src/lib.rs
@@ -1,21 +1,6 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_resturant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
