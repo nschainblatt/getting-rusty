99791b4fd91baecc88628dcda9d0f5905b86c8b1
nschainblatt <nschainblatt@gmail.com>
2024-01-07 21:23:48 -0500

    Code along for error handling, including Result types and ? operator

--- code-along/errors/src/main.rs
+++ code-along/errors/src/main.rs
@@ -1,8 +1,43 @@
-fn main() {
-    panic!("Crash and burn.");
-    
-    // Accessing an index out of bounds causing a panic
-    let v = vec![1, 2, 3];
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
 
-    v[99];
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn main() {
+    // panic!("Crash and burn.");
+    // Accessing an index out of bounds causing a panic
+    // let v = vec![1, 2, 3];
+    // v[99];
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file_result = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
 }
