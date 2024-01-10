b87ad41083fad62089b8be5af82034199935b0a2
nschainblatt <nschainblatt@gmail.com>
2024-01-09 23:07:29 -0500

    Update code along error file

--- code-along/errors/src/main.rs
+++ code-along/errors/src/main.rs
@@ -1,5 +1,15 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{self, ErrorKind, Read};
+
+fn read_username_from_file4() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut username = String::new();
+    let mut username_file = File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
 
 fn read_username_from_file2() -> Result<String, io::Error> {
     let mut username_file = File::open("hello.txt")?;
