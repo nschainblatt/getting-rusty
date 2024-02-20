4a2ab4846052f708fcec03da631ccc6ec83408ed
nschainblatt <nschainblatt@gmail.com>
2024-02-19 21:13:55 -0500

    Update code for oop concepts

--- code-along/blog/src/lib.rs
+++ code-along/blog/src/lib.rs
@@ -1,129 +1,67 @@
+pub struct DraftPost {
+    content: String,
+}
+
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text)
+    }
+
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
+        }
+    }
+}
+
+pub struct PendingReviewPost {
+    content: String,
+}
+
+impl PendingReviewPost {
+    pub fn schedule(self) -> ScheduledPost {
+        ScheduledPost {
+            content: self.content,
+        }
+    }
+
+    pub fn reject(self) -> DraftPost {
+        DraftPost {
+            content: self.content,
+        }
+    }
+}
+
+pub struct ScheduledPost {
+    content: String,
+}
+
+impl ScheduledPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
+    }
+
+    pub fn reject(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
+        }
+    }
+}
+
 pub struct Post {
-    state: Option<Box<dyn State>>,
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost {
             content: String::new(),
         }
     }
 
-    pub fn add_text(&mut self, text: &str) {
-        if let Some(state) = self.state.as_ref() {
-            self.content.push_str(state.add_text(text))
-        }
-    }
-
     pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(self)
-    }
-
-    pub fn request_review(&mut self) {
-        if let Some(state) = self.state.take() {
-            self.state = Some(state.request_review());
-        }
-    }
-
-    pub fn approve(&mut self) {
-        if let Some(state) = self.state.take() {
-            self.state = Some(state.approve());
-        }
-    }
-
-    pub fn reject(&mut self) {
-        if let Some(state) = self.state.take() {
-            self.state = Some(state.reject());
-        }
-    }
-}
-
-trait State {
-    fn add_text<'a>(&'a self, _text: &'a str) -> &'a str {
-        ""
-    }
-    fn request_review(self: Box<Self>) -> Box<dyn State>;
-    fn reject(self: Box<Self>) -> Box<dyn State>;
-    fn approve(self: Box<Self>) -> Box<dyn State>;
-    fn content<'a>(&self, _post: &'a Post) -> &'a str {
-        ""
-    }
-}
-
-struct Draft {}
-
-impl State for Draft {
-    fn add_text<'a>(&'a self, text: &'a str) -> &'a str {
-        text
-    }
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview {})
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-}
-
-struct PendingReview {}
-
-impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Scheduled {})
-    }
-}
-
-struct Reject {}
-
-impl State for Reject {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Draft {})
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-}
-
-struct Scheduled {}
-
-impl State for Scheduled {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Published {})
-    }
-}
-
-struct Published {}
-
-impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        &post.content
+        &self.content
     }
 }
--- code-along/blog/src/main.rs
+++ code-along/blog/src/main.rs
@@ -3,26 +3,17 @@ use blog::Post;
 fn main() {
     let mut post = Post::new();
 
-    // Draft state
     post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
-    println!("Draft state {}", post.content());
 
-    // Request review state
-    post.request_review();
-    post.add_text("blah");
-    assert_eq!("", post.content());
-    println!("Request review state {}", post.content());
+    let post = post.request_review();
+    let post = post.reject();
+    let post = post.request_review();
 
-    // Scheduled state
-    post.approve();
-    post.add_text("blah");
-    assert_eq!("", post.content());
+    let post = post.schedule();
+    let post = post.reject();
+    let post = post.schedule();
+
+    let post = post.approve();
+
     println!("Scheduled state {}", post.content());
-
-    // Approved state
-    post.approve();
-    post.add_text("blah");
-    assert_eq!("I ate a salad for lunch today", post.content());
-    println!("Approved state {}", post.content());
 }
