7e955cac90ea0d4f30af4bf66fc4bccbe2c3dcbf
nschainblatt <nschainblatt@gmail.com>
2024-02-21 07:27:24 -0500

    Add first version of thread pool (mine)

--- projects/web_server/src/main.rs
+++ projects/web_server/src/main.rs
@@ -2,15 +2,43 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread::{self, Builder, Thread},
+    time::Duration,
 };
+
+struct ThreadPool {
+    threads: Vec<Builder>,
+}
+
+impl ThreadPool {
+    fn new(num_threads: i32) -> ThreadPool {
+        let mut pool = ThreadPool { threads: vec![] };
+        for _ in 0..num_threads {
+            let thread = thread::Builder::new();
+            pool.threads.push(thread);
+        }
+        pool
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let mut pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
+        println!("Amount of available threads: {}", pool.threads.len());
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        let builder = pool.threads.pop().unwrap();
+
+        let _handler = builder
+            .spawn(|| {
+                handle_connection(stream);
+            })
+            .unwrap();
+
+        let thread = thread::Builder::new();
+        pool.threads.push(thread);
     }
 }
 
@@ -22,12 +50,15 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let request_line = &http_request[0];
+    let request_line = &http_request[0][..];
     println!("{request_line}");
-    let (status_line, filename) = if *request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match request_line {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
